62ea36b2b2e625ed96aa939f8b005fece1f7d6cf
H3NRY <h3nryguilh3rm3@gmail.com>
2025-11-11 18:34:48 -0300

    update
    ajeitando as telas, quase finalizando a tela do aluno referente as aulas

--- secondary-pages/main-aulas/script.js
+++ secondary-pages/main-aulas/script.js
@@ -15,20 +15,3 @@ overlay.addEventListener("click", () => {
   menuLateral.classList.remove("active");
   overlay.classList.remove("active");
 });
-
-// botões do main 
-
-// curso
-document.getElementById("btnCursos").addEventListener("click", () => {
-  window.location.href = "../secondary-pages/main-curso/index.html";
-});
-
-// aulas
-document.getElementById("btnAulas").addEventListener("click", () => {
-  window.location.href = "../secondary-pages/main-aulas/index.html";
-});
-
-// notas
-document.getElementById("btnNotas").addEventListener("click", () => {
-  window.location.href = "../secondary-pages/main-notas/index.html";
-});
